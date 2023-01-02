180fc9a873104eea7de74c3fb2e2714395542933
e1230 <embeltrans@correo.udistrital.edu.co>
2023-01-02 18:40:07 -0500

    configuracion inicial socket.io

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 require('dotenv').config();
+
+//node server
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//Mensaje de sockets
+io.on('connection', client => {
+    console.log('cliente conectado')
+    client.on('disconnect', () => { console.log('cliente desconectado') });
+});
 //path publico
 const publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT, (err) => {
+server.listen(process.env.PORT, (err) => {
     if (err) throw new Error(err);
     console.log('Servidor corriendo en puerto', 3001)
 });
