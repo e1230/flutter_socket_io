acfb7794186fb988c91325fd4489fd1f76e2e7a5
e1230 <embeltrans@correo.udistrital.edu.co>
2023-01-02 18:52:28 -0500

    emitir y escuchar eventos

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const io = require('socket.io')(server);
 io.on('connection', client => {
     console.log('cliente conectado')
     client.on('disconnect', () => { console.log('cliente desconectado') });
+    client.on('mensaje', (payload) => {
+        console.log('mensaje!!!', payload.nombre);
+        io.emit('mensaje', { admin: 'nuevo mensaje' })
+    })
 });
 //path publico
 const publicPath = path.resolve(__dirname, 'public');
