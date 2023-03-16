32c236a2ab473361c4479273d6a74eafb0a57e61
E1230 <embeltrans@correo.udistrital.edu.co>
2023-03-16 13:05:08 -0500

    agregar y eliminar banda

--- sockets/socket.js
+++ sockets/socket.js
@@ -20,7 +20,15 @@ io.on('connection', client => {
     client.on('vote-band', (payload) => {
         bands.voteBand(payload.id);
         io.emit('active-bands', bands.getBands())
-    })
+    });
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name))
+        io.emit('active-bands', bands.getBands())
+    });
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id)
+        io.emit('active-bands', bands.getBands())
+    });
     // client.on('nuevo-mensaje', (payload) => {
 
     //     io.emit('nuevo-mensaje', payload)
